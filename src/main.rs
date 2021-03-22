83e37368184bfef263d2ae15c7c07a5bc2c8a0b8
Corrina Sivak <csivak345@gmail.com>
2021-03-22 15:28:19 -0400

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+pub struct Pong;
+
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config = app_root.join("config/display.ron");
+    let assets = app_root.join("assets");
+
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(RenderToWindow::from_config_path(display_config)?.with_clear([0, 0, 0, 1]))
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+    
+    let mut game = Application::new(assets, Pong, game_data)?; // amethyst root object
+
+    game.run();
+
+    Ok(())
 }
